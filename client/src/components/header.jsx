fb36f83dbf4f0d6ac5d32891d55de63199f3c517
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2022-08-04 03:56:58 +0530

    Commit 013: Nav Bar Functionality Added

--- client/src/components/header.jsx
+++ client/src/components/header.jsx
@@ -1,60 +1,81 @@
-import React from 'react';
+import React, {useState} from 'react';
 import '../styles/header.css';
 import $ from 'jquery';
 
 function Header(){
 
-  function responsiveBar(){
-    var x = document.getElementById("navBar");
-    if (x.className === "nav-bar") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
+  const [style, setStyle] = useState("hide");
+  const [searchStyle, setSearchStyle] = useState("hide");
+
+  function responsiveBarOpen(){
+    setStyle("display");
+  }
+
+  function responsiveBarClose(){
+    setStyle("hide");
+  }
+
+  function openSearch(){
+    if(searchStyle === "hide"){
+      setSearchStyle("display");
+    }
+    if(searchStyle === "display"){
+      setSearchStyle("hide");
+    }
+    if(style === "display"){
+      setStyle("hide");
     }
   }
 
-  function searchBar(){
-    var y = document.getElementsByClassName("search-box");
-    if (y.style.visibility === 'hidden') {
-      y.style.visibility = 'visible';
-    } else {
-      y.style.visibility = 'hidden';
+  function closeSearch(){
+    if(searchStyle === "hide"){
+      setSearchStyle("hide");
     }
-
+    if(searchStyle === "display"){
+      setSearchStyle("hide");
+    }
   }
     
         return (
           <div class="header" id="header">
+
+            <div className={style} id="navDiv">
+            <button className='close-btn' onClick={responsiveBarClose}>X</button>
+            <a className='nav-div-link' href='/'><button className='nav-div-btn'>Home</button></a>
+            <a className='nav-div-link' href='/'><button className='nav-div-btn'>Blog</button></a>
+            <a className='nav-div-link' href='/'><button className='nav-div-btn'>Services</button></a>
+            <a className='nav-div-link' href='/'><button className='nav-div-btn'>About</button></a>
+            <a className='nav-div-link' href='/'><button className='nav-div-btn'>Sign in</button></a>
+            <a className='nav-div-link' href='#' onClick={openSearch}><button className='nav-div-btn'>&#128269;</button></a>
+            </div>
+
+
             <img class="site-logo" alt="logo" src="media/icon1.png"></img>
             <h1 class="site-title">swiftConnect</h1>
             <div class="nav-bar" id="navBar">
-            <a class="nav-link not div-link" onclick={searchBar} href="">&#128269;</a>
+            <a class="nav-link not div-link" href='#' onClick={openSearch}>&#128269;</a>
             <a class="nav-link not div-link" href='/signin'>Sign in</a>
             <a class="nav-link not div-link" href='/about'>About</a>
             <a class="nav-link not div-link" href='/services'>Services</a>
             <a class="nav-link not div-link" href='/articles'>Blog</a>
             <a class="nav-link active not div-link" href='/'>Home</a>
-            <a href=" " class="nav-link icon" onclick={responsiveBar}>
+            <a href="#" class="nav-link icon" onClick={responsiveBarOpen}>
             <i class="fa fa-bars"></i>
             </a>
             </div>
-            <div class="search-box" id="SearchBox">
-            <form class="search-form" method='post' action='/search' placeholder='type here to search' autofocus>
-            <input class="search-query" type="text" name='searchItem'></input>
-            <button class="search-btn">&#128269;</button>
+
+
+            <div className={searchStyle} id="SearchBox">
+            <form className="search-form" method='post' action='/search' autofocus>
+            <input className="search-query" type="text" name='searchItem' placeholder='type here to search' ></input>
+            <button className="search-btn">&#128269;</button>
+            <a className="search-btn" onClick={closeSearch}>X</a>
             </form>
-            
             </div>
-            {/* <div class="nav-div">
-            <a class="nav-link active not" href='/'>Home</a>
-            <a class="nav-link not" href='/articles'>Blog</a>
-            <a class="nav-link not" href='/services'>Services</a>
-            <a class="nav-link not" href='/about'>About</a>
-            <a class="nav-link not" href='/signin'>Sign in</a>
-            <a class="nav-link not" href='/search'>&#128269;</a>
-            </div> */}
 
           </div>
+
+          
         );
       
 }
