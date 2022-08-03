0297dab3d61be8e82cc795981cfe3b11d19c6ef8
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2022-08-03 15:25:46 +0530

    Commit 005: Landing Section Complete

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -4,7 +4,13 @@ import '../styles/landing.css';
 function Landing(){
     return(
         <div className="landing-section">
-        <h1>Hi I am landing section!</h1>
+        <div className="tagline-box">
+        <h1 className="tagline-title">Helps you to securely connect to the world over internet</h1>
+        <p className="tagline-para">We provide security from cyber threats and a secure internet service.</p>
+        </div>
+        <div className="illustration-box">
+        <img className="landing-illustration" src="media/2.png" alt="Cyber Security"></img>
+        </div>
         </div>
     );
 }
