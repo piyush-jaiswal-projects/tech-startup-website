e2fa195badac995b1f14b7b85571c041e1b88970
Piyush Jaiswal <jaiswalpiyushaerospacex@gmail.com>
2022-08-03 14:23:35 +0530

    Commit 004: Navbar bug fixed

--- client/src/components/landing.jsx
+++ client/src/components/landing.jsx
@@ -4,7 +4,7 @@ import '../styles/landing.css';
 function Landing(){
     return(
         <div className="landing-section">
-        
+        <h1>Hi I am landing section!</h1>
         </div>
     );
 }
