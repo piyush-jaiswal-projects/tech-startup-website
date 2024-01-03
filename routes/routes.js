21306f2d39a54ec5eb13970ca708205e7f9bd744
Piyush Jaiswal <piyushjaiswal1610@gmail.com>
2024-01-03 18:40:23 +0530

    dfwdd

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,11 @@ const router = express.Router();
 const register = require('../controllers/register');
 const login = require('../controllers/login');
 
+router.get('/', (req, res) => {
+    res.status(200).json({
+        message: "welcome!"
+    })
+})
 router.post('/register', register);
 router.post('/login', login);
 
